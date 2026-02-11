61797651567ec2038143bd717d6168fe6187d778
Mahira garg <157530747+Mahira1111garg@users.noreply.github.com>
2026-02-11 23:08:53 +0530

    Time: 14 ms (51.34%), Space: 42 MB (70.61%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,17 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left=0;
+        int right=n;
+        while(left<right){
+            int mid=left-(left-right)/2;
+            if(isBadVersion(mid)){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
+
+    }
+}
